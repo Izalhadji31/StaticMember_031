0502e67bbfe29073cd9a6d45c1bd0c41b8c5d65a
Izalhadji31 <ryzalhadji@gmail.com>
2023-05-17 21:25:41 +0700

    membuat int main

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -36,3 +36,9 @@ void mahasiswa::cetak() {
 	cout << endl << "NIM = " << nim << endl;
 	cout << "Nama = " << nama << endl;
 }
+
+int main() {
+	mahasiswa mhs1;
+	mahasiswa mhs2(20);
+	mahasiswa mhs3("Indra");
+	mahasiswa mhs4(30, "Fauzan");
