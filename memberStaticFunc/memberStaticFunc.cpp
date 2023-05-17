8b2e40c3b63918162e71a5508dc97673fb3bcce3
Izalhadji31 <ryzalhadji@gmail.com>
2023-05-17 21:11:53 +0700

    membuat class mahasiwa prog 2

---
+++ memberStaticFunc/memberStaticFunc.cpp
@@ -0,0 +1,17 @@
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+	static void setNim(int pnim) { nim = pnim; /*Definisi function*/ }
+	static int getNim() { return nim; /*Definisi Function*/ }
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
