8fea9bfaf965951227f05a33ae51188caaed2991
Izalhadji31 <ryzalhadji@gmail.com>
2023-05-17 21:20:19 +0700

    memanggil method buku set judul

--- chainfuction/chainfuction.cpp
+++ chainfuction/chainfuction.cpp
@@ -15,4 +15,7 @@ public:
 
 int main() {
 	// bukunya.setJudul("Matematika");
+	// cout << bukunya.getJudul();
+	cout << bukunya.setJudul("Matematika").getJudul(); //chain function calls
+	return 0;
 }
